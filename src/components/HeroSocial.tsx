b91b811c1872b47aa84ffa7ff5d6a4392bd48bec
DivyanshuBhoyar <divyanshubhoyar@yahoo.com>
2022-07-29 23:27:22 +0530

    feat: heroicon custom classes

--- src/components/HeroSocial.tsx
+++ src/components/HeroSocial.tsx
@@ -1,11 +1,12 @@
 type IHeroSocialProps = {
   src: string;
   alt: string;
+  className?: string;
 };
 
 const HeroSocial = (props: IHeroSocialProps) => (
   <img
-    className="h-12 w-12 hover:translate-y-1"
+    className={`h-12 w-12 hover:translate-y-1 ${props.className} `}
     src={props.src}
     alt={props.alt}
     loading="lazy"
